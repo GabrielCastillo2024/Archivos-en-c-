a873bf87843a4f644a14ff91c40c0e19450fa63f
GabrielCastillo2024 <gecastilloc@uamv.edu.ni>
2024-10-04 10:54:23 -0600

    Agregamos el codigo hecho

---
+++ Archivos/Archivos con interfaz/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Archivos_con_interfaz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbAge = new System.Windows.Forms.TextBox();
+            this.tbNote = new System.Windows.Forms.TextBox();
+            this.tbGenero = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.dgvPersona = new System.Windows.Forms.DataGridView();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersona)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(67, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(83, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Edad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(83, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nota";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(169, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ingrese su genero";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(207, 21);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(134, 22);
+            this.tbName.TabIndex = 4;
+            // 
+            // tbAge
+            // 
+            this.tbAge.Location = new System.Drawing.Point(207, 60);
+            this.tbAge.Name = "tbAge";
+            this.tbAge.Size = new System.Drawing.Size(134, 22);
+            this.tbAge.TabIndex = 5;
+            // 
+            // tbNote
+            // 
+            this.tbNote.Location = new System.Drawing.Point(207, 100);
+            this.tbNote.Name = "tbNote";
+            this.tbNote.Size = new System.Drawing.Size(134, 22);
+            this.tbNote.TabIndex = 6;
+            // 
+            // tbGenero
+            // 
+            this.tbGenero.Location = new System.Drawing.Point(207, 140);
+            this.tbGenero.Name = "tbGenero";
+            this.tbGenero.Size = new System.Drawing.Size(134, 22);
+            this.tbGenero.TabIndex = 7;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(478, 32);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(87, 31);
+            this.btnAdd.TabIndex = 9;
+            this.btnAdd.Text = "Agregar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(478, 100);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(87, 34);
+            this.btnLeer.TabIndex = 10;
+            this.btnLeer.Text = "Leer";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // dgvPersona
+            // 
+            this.dgvPersona.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPersona.Location = new System.Drawing.Point(12, 222);
+            this.dgvPersona.Name = "dgvPersona";
+            this.dgvPersona.RowHeadersWidth = 51;
+            this.dgvPersona.RowTemplate.Height = 24;
+            this.dgvPersona.Size = new System.Drawing.Size(768, 216);
+            this.dgvPersona.TabIndex = 11;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(478, 162);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(87, 35);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Limpiar";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.dgvPersona);
+            this.Controls.Add(this.btnLeer);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbGenero);
+            this.Controls.Add(this.tbNote);
+            this.Controls.Add(this.tbAge);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersona)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbAge;
+        private System.Windows.Forms.TextBox tbNote;
+        private System.Windows.Forms.TextBox tbGenero;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.DataGridView dgvPersona;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Archivos/Archivos con interfaz/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Archivos_con_interfaz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            FileStream mArchivoEscritor = new FileStream("datos.dat", FileMode.OpenOrCreate, FileAccess.Write);
+            using (BinaryWriter Escritor = new BinaryWriter(mArchivoEscritor)) { 
+
+                string nombre = tbName.Text;
+            int edad = int.Parse(tbAge.Text);
+            string nota = tbNote.Text;
+            string genero = tbGenero.Text;
+
+            using (StreamWriter personas = new StreamWriter("personas.txt", true))
+            {
+                personas.WriteLine($"{nombre},{edad},{nota},{genero}");
+                
+            }
+
+            MessageBox.Show("Datos agregados correctamente.");}
+
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            List<Persona> personas = new List<Persona>();
+
+            using (StreamReader sr = new StreamReader("personas.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(',');
+                    personas.Add(new Persona
+                    {
+                        Nombre = parts[0],
+                        Edad = int.Parse(parts[1]),
+                        Nota = parts[2],
+                        Genero = parts[3]
+                    });
+                }
+            }
+
+            dgvPersona.DataSource = personas;
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbName.Clear();
+            tbAge.Clear();
+            tbNote.Clear();
+            tbGenero.Clear();
+            dgvPersona.DataSource = null;
+
+        }
+    }
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Nota { get; set; }
+        public string Genero { get; set; }
+    }
+}
